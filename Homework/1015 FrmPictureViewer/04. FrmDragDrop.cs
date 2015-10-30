f992331633a146dd7e2c4d491c6f1bd2d846de46
Lily <s8012162004@yahoo.com.tw>
2015-10-30 21:03:38 +0800

    1030

--- Homework/1015 FrmPictureViewer/04. FrmDragDrop.cs
+++ Homework/1015 FrmPictureViewer/04. FrmDragDrop.cs
@@ -13,20 +13,55 @@ namespace Lab_02__Y_
 {
     public partial class FrmDragDrop : Form
     {
+        List<Control> ShowPicture = new List<Control>();
+        List<string> s = new List<string>();
+        int t = 0;
+        int h, w;
         public FrmDragDrop()
         {
             InitializeComponent();
             this.pictureBox1.AllowDrop = true;
             this.pictureBox1.DragEnter += pictureBox1_DragEnter;
             this.pictureBox1.DragDrop += pictureBox1_DragDrop;
+            this.flowLayoutPanel1.AllowDrop = true;
+            this.flowLayoutPanel1.DragEnter += flowLayoutPanel1_DragEnter;
+            this.flowLayoutPanel1.DragDrop += flowLayoutPanel1_DragDrop;
+            this.openFileDialog1.InitialDirectory = "c:\\User\\III\\圖片";
+            this.openFileDialog1.Filter = "Image|*.jpg;*.bmp;*.gif;*.png|JPeg Image|*.jpg|Bitmap Image|*.bmp|Gif Image|*.gif|Png Image|*.png";
             foreach (Control x in this.flowLayoutPanel1.Controls)
             {
                 x.MouseDown += Source_pictureBox_MouseDown;
             }
-                        
+            h = this.pictureBox1.Height;
+            w = this.pictureBox1.Width;            
         }
 
-       
+        void flowLayoutPanel1_DragDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string[] filenames = (string[])e.Data.GetData(DataFormats.FileDrop);
+                for (int i = 0; i < filenames.Length; i++)
+                {
+                    PictureBox p = new PictureBox();
+                    p.Width = 120;
+                    p.Height = 90;
+                    p.SizeMode = PictureBoxSizeMode.StretchImage;
+                    p.Image = Image.FromFile(filenames[i]);
+                    this.flowLayoutPanel1.Controls.Add(p);
+                }
+            }
+            foreach (Control x in this.flowLayoutPanel1.Controls)
+            {
+                x.MouseDown += Source_pictureBox_MouseDown;
+            }
+        }
+
+        void flowLayoutPanel1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Copy;
+        }
+
         private void Source_pictureBox_MouseDown(object sender, MouseEventArgs e)
         {
             //this.DoDragDrop(((PictureBox)sender).Image, DragDropEffects.Copy);
@@ -39,7 +74,6 @@ namespace Lab_02__Y_
             }
         }
 
-        
         void pictureBox1_DragDrop(object sender, DragEventArgs e)
         {
             //MessageBox.Show("Drag Drop!");
@@ -59,16 +93,106 @@ namespace Lab_02__Y_
             e.Effect = DragDropEffects.Copy;
         }
 
-        private void button2_Click(object sender, EventArgs e)
+        private void Clear_Click(object sender, EventArgs e)
         {
             this.pictureBox1.Image = null;
         }
 
         private void toolStripButton1_Click(object sender, EventArgs e)
         {
+            ShowPicture.Clear();
+            foreach (Control x in this.flowLayoutPanel1.Controls)
+            {
+                ShowPicture.Add(x);
+            }
+            for (int i = 0; i < this.ShowPicture.Count; i++)
+            {
+                if (this.pictureBox1.Image == ((PictureBox)this.ShowPicture[i]).Image)
+                {
+                    if (i-1 < 0)
+                    {
+                        i = this.ShowPicture.Count;
+                    }
+                    this.pictureBox1.Image = ((PictureBox)this.ShowPicture[i - 1]).Image;
+                    return;
+                }
+            }
+        }
+
+        private void toolStripButton2_Click(object sender, EventArgs e)
+        {
+            ShowPicture.Clear();
+            foreach (Control x in this.flowLayoutPanel1.Controls)
+            {
+                ShowPicture.Add(x);
+            }
+            for (int i = 0; i < this.ShowPicture.Count; i++)
+            {
+                if (this.pictureBox1.Image == ((PictureBox)this.ShowPicture[i]).Image)
+                {
+                    if (i + 1 == this.ShowPicture.Count)
+                    {
+                        i = -1;
+                    }
+                    this.pictureBox1.Image = ((PictureBox)this.ShowPicture[i + 1]).Image;
+                    return;
+                }
+            }
+        }
+
+        private void toolStripButton3_Click(object sender, EventArgs e)
+        {
+            this.timer1.Enabled = !this.timer1.Enabled;
+        }
+
+        private void Openfile_Click(object sender, EventArgs e)
+        {
+            if (this.openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                string[] a = this.openFileDialog1.FileNames;
+                foreach (var item in a)
+                {
+                    this.s.Add(item);
+                }                
+                this.AddPicturebox(this.s);
+                this.s.Clear();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (this.timer1.Enabled == true)
+            {
+                this.show_picturebox();
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            if (this.trackBar1.Value == 0)
+            {
+                this.pictureBox1.Height = h;
+                this.pictureBox1.Width = w;
+            }
+            if (this.trackBar1.Value != 0)
+            {
+                this.pictureBox1.Height = h * (this.trackBar1.Value * 2 + 1);
+                this.pictureBox1.Width = w * (this.trackBar1.Value * 2 + 1);
+            }
 
         }
-        List<Control> ShowPicture = new List<Control>();
+
+        private void trackBar1_MouseUp(object sender, MouseEventArgs e)
+        {
+            this.timer2.Enabled = false;
+        }
+
+        private void trackBar1_MouseDown(object sender, MouseEventArgs e)
+        {
+           
+            this.timer2.Enabled = true;
+        }
+             
         void show_picturebox()
         {
             ShowPicture.Clear();
@@ -76,36 +200,31 @@ namespace Lab_02__Y_
             {
                 ShowPicture.Add(x);
             }
-            for (int i = 0; i < ShowPicture.Count; i++)
+            if (this.t < this.ShowPicture.Count)
             {
-               this.pictureBox1.Image = ((PictureBox)ShowPicture[i]).Image;  
+                this.pictureBox1.Image = ((PictureBox)ShowPicture[this.t]).Image;
+                this.t++;
             }
+            else 
+            {
+                this.t = 0;
+            }
+            
         }
-        void AddPicturebox()
+
+        void AddPicturebox( List<string>x)
         { 
-            for (int i = 0; i < this.s.Count ; i++)
+            for (int i = 0; i < x.Count ; i++)
 			{
                 PictureBox p = new PictureBox();
                 p.Width = 120;
                 p.Height = 90;
                 p.SizeMode = PictureBoxSizeMode.StretchImage;
-                p.Image = Image.FromFile(s[i]);
+                p.Image = Image.FromFile(x[i]);
                 this.flowLayoutPanel1.Controls.Add(p);
 			}
          
         }
-        List<string> s = new List<string>();
-        private void button1_Click(object sender, EventArgs e)
-        {
-            if (this.openFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK)
-            {
-                //TODO...開項目數目
-                s.Add(this.openFileDialog1.FileName);
-                this.AddPicturebox();
-                s.Clear();
-            }
-        }
-
         
     }
 }
--- Homework/1015 FrmPictureViewer/04. FrmDragDrop.designer.cs
+++ Homework/1015 FrmPictureViewer/04. FrmDragDrop.designer.cs
@@ -28,11 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmDragDrop));
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button2 = new System.Windows.Forms.Button();
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.panel1 = new System.Windows.Forms.Panel();
             this.toolStrip1 = new System.Windows.Forms.ToolStrip();
             this.toolStripButton1 = new System.Windows.Forms.ToolStripButton();
             this.toolStripButton2 = new System.Windows.Forms.ToolStripButton();
@@ -46,11 +48,14 @@
             this.pictureBox7 = new System.Windows.Forms.PictureBox();
             this.trackBar1 = new System.Windows.Forms.TrackBar();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
+            this.panel1.SuspendLayout();
             this.toolStrip1.SuspendLayout();
             this.flowLayoutPanel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
@@ -70,17 +75,14 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "Open File...";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.Openfile_Click);
             // 
             // pictureBox1
             // 
-            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Location = new System.Drawing.Point(47, 18);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(658, 395);
+            this.pictureBox1.Size = new System.Drawing.Size(600, 400);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -93,7 +95,9 @@
             this.button2.TabIndex = 2;
             this.button2.Text = "Clear";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.Clear_Click);
+            this.button2.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragDrop);
+            this.button2.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragEnter);
             // 
             // splitContainer1
             // 
@@ -109,10 +113,24 @@
             // 
             // splitContainer1.Panel2
             // 
-            this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
-            this.splitContainer1.Size = new System.Drawing.Size(860, 560);
+            this.splitContainer1.Panel2.Controls.Add(this.panel1);
+            this.splitContainer1.Size = new System.Drawing.Size(860, 599);
             this.splitContainer1.SplitterDistance = 130;
             this.splitContainer1.TabIndex = 5;
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(720, 435);
+            this.panel1.TabIndex = 2;
             // 
             // toolStrip1
             // 
@@ -122,7 +140,7 @@
             this.toolStripButton1,
             this.toolStripButton2,
             this.toolStripButton3});
-            this.toolStrip1.Location = new System.Drawing.Point(0, 521);
+            this.toolStrip1.Location = new System.Drawing.Point(0, 560);
             this.toolStrip1.Name = "toolStrip1";
             this.toolStrip1.Size = new System.Drawing.Size(860, 39);
             this.toolStrip1.TabIndex = 6;
@@ -144,6 +162,7 @@
             this.toolStripButton2.Name = "toolStripButton2";
             this.toolStripButton2.Size = new System.Drawing.Size(79, 36);
             this.toolStripButton2.Text = "下一頁";
+            this.toolStripButton2.Click += new System.EventHandler(this.toolStripButton2_Click);
             // 
             // toolStripButton3
             // 
@@ -152,22 +171,27 @@
             this.toolStripButton3.Name = "toolStripButton3";
             this.toolStripButton3.Size = new System.Drawing.Size(91, 36);
             this.toolStripButton3.Text = "連續撥放";
+            this.toolStripButton3.Click += new System.EventHandler(this.toolStripButton3_Click);
             // 
             // flowLayoutPanel1
             // 
             this.flowLayoutPanel1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.flowLayoutPanel1.Controls.Add(this.pictureBox2);
             this.flowLayoutPanel1.Controls.Add(this.pictureBox3);
             this.flowLayoutPanel1.Controls.Add(this.pictureBox4);
             this.flowLayoutPanel1.Controls.Add(this.pictureBox5);
             this.flowLayoutPanel1.Controls.Add(this.pictureBox6);
             this.flowLayoutPanel1.Controls.Add(this.pictureBox7);
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 401);
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 440);
             this.flowLayoutPanel1.Name = "flowLayoutPanel1";
             this.flowLayoutPanel1.Size = new System.Drawing.Size(860, 120);
             this.flowLayoutPanel1.TabIndex = 7;
+            this.flowLayoutPanel1.DragDrop += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragDrop);
+            this.flowLayoutPanel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.pictureBox1_DragEnter);
+            this.flowLayoutPanel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Source_pictureBox_MouseDown);
             // 
             // pictureBox2
             // 
@@ -232,21 +256,34 @@
             // trackBar1
             // 
             this.trackBar1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.trackBar1.Location = new System.Drawing.Point(510, 515);
+            this.trackBar1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.trackBar1.Location = new System.Drawing.Point(512, 554);
             this.trackBar1.Name = "trackBar1";
             this.trackBar1.Size = new System.Drawing.Size(347, 45);
             this.trackBar1.TabIndex = 8;
+            this.trackBar1.TickStyle = System.Windows.Forms.TickStyle.Both;
+            this.trackBar1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.trackBar1_MouseDown);
+            this.trackBar1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.trackBar1_MouseUp);
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
             this.openFileDialog1.Multiselect = true;
             // 
+            // timer1
+            // 
+            this.timer1.Interval = 500;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
             // FrmDragDrop
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(860, 560);
+            this.ClientSize = new System.Drawing.Size(860, 599);
             this.Controls.Add(this.trackBar1);
             this.Controls.Add(this.toolStrip1);
             this.Controls.Add(this.flowLayoutPanel1);
@@ -258,6 +295,7 @@
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
             this.toolStrip1.ResumeLayout(false);
             this.toolStrip1.PerformLayout();
             this.flowLayoutPanel1.ResumeLayout(false);
@@ -292,5 +330,8 @@
         private System.Windows.Forms.PictureBox pictureBox7;
         private System.Windows.Forms.TrackBar trackBar1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Panel panel1;
     }
 }
